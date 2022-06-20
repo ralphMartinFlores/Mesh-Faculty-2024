43f9b4c00b4b01a6735fcd8b7a1076d898b29241
Ralph Martin Flores <201811044@gordoncollege.edu.ph>
2022-06-20 17:10:35 +0800

    Create group dialog added

---
+++ gclampv3/faculty/faculty/src/app/modules/group-messaging/create-group/create-group.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-create-group',
+  templateUrl: './create-group.component.html',
+  styleUrls: ['./create-group.component.scss']
+})
+export class CreateGroupComponent implements OnInit {
+
+  toppings = new FormControl('');
+  toppingList: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- gclampv3/faculty/faculty/src/app/modules/group-messaging/group-messaging.component.ts
+++ gclampv3/faculty/faculty/src/app/modules/group-messaging/group-messaging.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CreateGroupComponent } from './create-group/create-group.component';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-group-messaging',
@@ -9,9 +11,28 @@ export class GroupMessagingComponent implements OnInit {
 
   public grouparray = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1];
 
-  constructor() { }
+  constructor(public _dialog: MatDialog) { }
 
   ngOnInit(): void {
+  }
+
+  // createGroupDialog() {
+  //   const dialogConfig = new MatDialogConfig();
+  //   this._dialog.open(CreateGroupComponent, dialogConfig {
+  //     maxHeight: '80vh'
+  //   });
+  // }
+
+  public createGroupDialog (): void {
+    let dialogRef = this._dialog.open(CreateGroupComponent, {
+      maxHeight: "85vh",
+      maxWidth: "90vw"
+    });
+
+    dialogRef.afterClosed().subscribe(participant => {
+      // console.log('closed');
+    });
+
   }
 
 }
